1be022a6202be3c559e09d707eb2d98de42860c5
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-18 13:22:45 +0530

    Create Set58.c

---
+++ Set58.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,k,a[30],des=0,i;
+clrscr();
+scanf("%d%d",&n1,&k);
+for(i=0;i<n1;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n1;i++)
+{
+if(a[i]==k)
+{
+printf("yes");
+des=0;
+break;
+}
+else
+{
+des=1;
+}
+}
+if(des==1)
+{
+printf("no");
+}
+getch();
+}
