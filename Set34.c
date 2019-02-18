12f4944e2cd12feb561ba71dcbcc1f8dfdb7b500
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-18 08:33:44 +0530

    Create Set34.c

---
+++ Set34.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char b[50];
+int i,count=1;
+clrscr();
+gets(a);
+for(i=0;b[i]!='\0';i++)
+{
+if(b[i]=='.')
+{
+count++;
+}
+}
+printf("%d",count);
+getch();
+}
