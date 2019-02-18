7039d2c0fd83e9381d7b0bf147e0f36b72958e6b
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-18 08:29:06 +0530

    Create Set32.c

---
+++ Set32.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a1[20];
+int j,c=1;
+clrscr();
+gets(a1);
+for(j=0;j<n;j++)
+{
+if(a1[j]==' ')
+{
+c++;
+}
+}
+printf("%d",c);
+getch();
+return 0;
+}
