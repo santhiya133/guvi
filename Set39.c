31edec48af1f52181ec6106f538fcea101a316bb
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-18 13:04:09 +0530

    Update Set39.c

--- Set39.c
+++ Set39.c
@@ -2,19 +2,19 @@
 #include<conio.h>
 void main()
 {
-int a[20],max,i,n;
+int b[20],max,i,n;
 clrscr();
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d",&b[i]);
 }
 max=0;
 for(i=0;i<n;i++)
 {
-if(a[i]>max)
+if(b[i]>max)
 {
-max=a[i];
+max=alb[i];
 }
 }
 printf("%d",max);
