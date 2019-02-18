13be021cc742ae37b961680513c706a45e754e08
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-18 08:19:33 +0530

    Create Guvi32.c

---
+++ Guvi32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a1[20];
+int i,count=1;
+clrscr();
+gets(a1);
+for(i=0;i<n;i++)
+{
+if(a1[i]==' ')
+{
+count++;
+}
+}
+printf("%d",count);
+getch();
+}
