4ad1f0fa9a792cdc8ac66bc665673215a26f1940
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-19 08:26:58 +0530

    Create Set73.c

---
+++ Set73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,n1;
+clrscr();
+scanf("%d%d%d",&n1,&a,&b);
+if(n1>a && n1<b)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
