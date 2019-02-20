74cfa8a3b0aee24b840ecfd75ddfeb009ac3fb13
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-20 13:15:47 +0530

    Create Set21.c

---
+++ Set21.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+{
+	int n1,a,d,s;
+	int N1,D,A,S;
+	printf("\n enter the number of terms:");
+	scanf("%d",&n1);
+	printf("\n enter the start value:");
+	scanf("%d",&a);
+	printf("\n enter the difference:");
+	scanf("%d",&d);
+	N1=n-1;
+	D=N1*d;
+	A=2*a;
+	S=A+D;
+	s=S*n1/2;
+	printf("\n%d",s);
+	return 0;
+}
