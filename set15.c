01b96c38df719e72b5ea9034696e528ca25931b4
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-20 01:46:35 -0800

    Update set15.c

--- set15.c
+++ set15.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int n,q,i=0;
-scanf("%d%d",&n,&q);
-for(i=n+1;i<q;i++)
+int n1,q1,i=0;
+scanf("%d%d",&n1,&q1);
+for(i=n1+1;i<q1;i++)
 {
-if(i%2!=0)
+if(i%2==0)
 {
 printf("%d",i);
 }
