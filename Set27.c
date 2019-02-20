cdd7b25ba8b11d2d92eaed9ae8d0cddebffdeca5
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-20 16:57:54 +0530

    Create Set27.c

---
+++ Set27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+#include<conio.h>
+void main()
+{
+char m;
+printf("enter the string:",n);
+scanf("%s",&m);
+if(m>=65&&m<=90||m>=97&&m<=122)
+{
+printf("no");
+}
+else
+{
+printf("yes");
+}
+getch();
+}
