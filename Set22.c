5fed29c03e851edd457fbf8f1796a0b41d81078e
santhiya133 <47082406+santhiya133@users.noreply.github.com>
2019-02-20 13:17:11 +0530

    Create Set22.c

---
+++ Set22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int arr[20],max,i,n;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&ar[i]);
+}
+max=0;
+for(i=0;i<n;i++)
+{
+if(ar[i]>max)
+{
+max=ar[i];
+}
+}
+printf("%d",max);
+getch();
+}
